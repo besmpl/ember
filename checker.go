b02b2adc89e0c80a7da8e239292193cfe3b4bd65
Mark <besmpl@icloud.com>
2026-07-05 21:48:21 +0300

    Separate table summary path helpers

--- checker.go
+++ checker.go
@@ -755,65 +755,6 @@ func getMetatableCallTypeSummary(call callExpression, values map[string]TypeSumm
 	return *table.Metatable
 }
 
-func tableSummaryProperty(summary TypeSummary, name string) (TypeSummary, bool) {
-	for _, property := range summary.Properties {
-		if property.Name == name {
-			return property.Type, true
-		}
-	}
-	return TypeSummary{}, false
-}
-
-func tableSummaryPath(summary TypeSummary, selectors []selector) (TypeSummary, bool) {
-	current := summary
-	for _, selector := range selectors {
-		if selector.field == "" || current.Kind != TypeSummaryTable {
-			return TypeSummary{}, false
-		}
-		next, ok := tableSummaryProperty(current, selector.field)
-		if !ok {
-			return TypeSummary{}, false
-		}
-		current = next
-	}
-	return current, true
-}
-
-func setTableSummaryProperty(summary *TypeSummary, name string, value TypeSummary) {
-	for i := range summary.Properties {
-		if summary.Properties[i].Name == name {
-			summary.Properties[i].Type = value
-			return
-		}
-	}
-	summary.Properties = append(summary.Properties, TablePropertySummary{
-		Name: name,
-		Type: value,
-	})
-}
-
-func setTableSummaryPath(summary *TypeSummary, selectors []selector, value TypeSummary) bool {
-	if len(selectors) == 0 {
-		return false
-	}
-	field := selectors[0].field
-	if field == "" {
-		return false
-	}
-	if len(selectors) == 1 {
-		setTableSummaryProperty(summary, field, value)
-		return true
-	}
-	for i := range summary.Properties {
-		property := &summary.Properties[i]
-		if property.Name != field || property.Type.Kind != TypeSummaryTable {
-			continue
-		}
-		return setTableSummaryPath(&property.Type, selectors[1:], value)
-	}
-	return false
-}
-
 func tableValueTypeSummary(table tableExpression, values map[string]TypeSummary) TypeSummary {
 	summary := TypeSummary{Kind: TypeSummaryTable, Display: "table"}
 	for _, field := range table.fields {
---
+++ type_summary_table.go
@@ -0,0 +1,60 @@
+package ember
+
+func tableSummaryProperty(summary TypeSummary, name string) (TypeSummary, bool) {
+	for _, property := range summary.Properties {
+		if property.Name == name {
+			return property.Type, true
+		}
+	}
+	return TypeSummary{}, false
+}
+
+func tableSummaryPath(summary TypeSummary, selectors []selector) (TypeSummary, bool) {
+	current := summary
+	for _, selector := range selectors {
+		if selector.field == "" || current.Kind != TypeSummaryTable {
+			return TypeSummary{}, false
+		}
+		next, ok := tableSummaryProperty(current, selector.field)
+		if !ok {
+			return TypeSummary{}, false
+		}
+		current = next
+	}
+	return current, true
+}
+
+func setTableSummaryProperty(summary *TypeSummary, name string, value TypeSummary) {
+	for i := range summary.Properties {
+		if summary.Properties[i].Name == name {
+			summary.Properties[i].Type = value
+			return
+		}
+	}
+	summary.Properties = append(summary.Properties, TablePropertySummary{
+		Name: name,
+		Type: value,
+	})
+}
+
+func setTableSummaryPath(summary *TypeSummary, selectors []selector, value TypeSummary) bool {
+	if len(selectors) == 0 {
+		return false
+	}
+	field := selectors[0].field
+	if field == "" {
+		return false
+	}
+	if len(selectors) == 1 {
+		setTableSummaryProperty(summary, field, value)
+		return true
+	}
+	for i := range summary.Properties {
+		property := &summary.Properties[i]
+		if property.Name != field || property.Type.Kind != TypeSummaryTable {
+			continue
+		}
+		return setTableSummaryPath(&property.Type, selectors[1:], value)
+	}
+	return false
+}
